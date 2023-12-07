7dc144442bcb97c1cd810770a6f784b020bb400d
scottdurica <scottdurica@gmail.com>
2023-12-06 22:32:54 -0500

    updated views

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+
+class Author(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    species = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.username
+    
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+
+
